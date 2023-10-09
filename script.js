c18e9f6503cd317a03cd06f5776963fb3950b020
Nate <nathaniel_ramirez@icloud.com>
2023-10-08 18:22:52 -0700

    redirect i f 404

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Stackoverflow didnt work and earned the L
 window.onload = function () {
+  var dontShowAgain = localStorage.getItem("dontShowMessageAgain");
+  var messageDiv = document.getElementById("message");
+  var checkBox = document.getElementById("dontShowAgain");
+  checkBox.checked = false; // Keeps it unchecked on page load
   document.getElementsByTagName("a").target = "_blank";
+
+  if (dontShowAgain === "true") {
+    messageDiv.style.display = "none";
+  }
 };
 
 // But this... it earned the W
@@ -23,21 +31,21 @@ function closeMessage() {
   messageDiv.style.display = "none";
 }
 
-window.onload = function () {
-  var dontShowAgain = localStorage.getItem("dontShowMessageAgain");
-  var messageDiv = document.getElementById("message");
-  var checkBox = document.getElementById("dontShowAgain");
-  checkBox.checked = false; // Keeps it unchecked on page load
-
-  if (dontShowAgain === "true") {
-    messageDiv.style.display = "none";
-  }
-  if (window.location.pathname === "/*") {
-    window.location.href = "/";
-  }
-};
-
 function getPath() {
   const path = document.getElementById("path");
   path.textContent = window.location.pathname;
+}
+
+// Redirect to root if 404
+const currentUrl = new URL(window.location.href);
+const isRedirected = !!currentUrl.searchParams.get("r");
+
+if (!isRedirected) {
+  fetch(window.location.href, {
+    method: "HEAD",
+  }).then((response) => {
+    if (response.status !== 404) return;
+
+    window.location.href = "/"; // https://www.davidangulo.xyz/posts/redirect-404-pages-in-javascript/
+  });
 }
